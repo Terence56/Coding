5d91a7e31cc9d34689de0de5c221d3b2f711b271
terence56 <terence198256@gmail.com>
2016-05-03 15:38:50 +0800

    型別&參考

--- Elephant/Elephant/Form1.Designer.cs
+++ Elephant/Elephant/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -42,6 +43,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Lloyd";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -52,6 +54,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Lucinda";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -62,12 +65,24 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "Swap!";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(101, 291);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 45);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(327, 296);
+            this.ClientSize = new System.Drawing.Size(345, 416);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -82,6 +97,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Elephant/Elephant/Form1.cs
+++ Elephant/Elephant/Form1.cs
@@ -12,9 +12,51 @@ namespace Elephant
 {
     public partial class Form1 : Form
     {
+        Elephant lloyd;
+        Elephant lucinda; 
         public Form1()
         {
             InitializeComponent();
+            lloyd = new Elephant()
+            {
+                Name = "Lucinda",
+                EarSize = 33
+            };
+            lucinda = new Elephant()
+            {
+                Name = "Lloyd",
+                EarSize = 40
+            };
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lloyd.WhoAmI();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lucinda.WhoAmI();
+            //呼叫方法
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Elephant holder;
+            holder = lloyd;
+            lloyd = lucinda;
+            lucinda = holder;
+            MessageBox.Show("Objects swapped!");
+            
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //lloyd = lucinda;
+            //會被複寫
+            lloyd.EarSize = 4321;
+            lloyd.WhoAmI();
         }
     }
 }
