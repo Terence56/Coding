6b4d0ebdcf7f76fd4682b9900954fbda2d0a059e
terence56 <terence198256@gmail.com>
2016-05-05 15:57:31 +0800

    P174

--- Elephant/Elephant/Form1.Designer.cs
+++ Elephant/Elephant/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -39,7 +41,7 @@
             this.button1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button1.Location = new System.Drawing.Point(101, 37);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 46);
+            this.button1.Size = new System.Drawing.Size(135, 45);
             this.button1.TabIndex = 0;
             this.button1.Text = "Lloyd";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,9 +50,9 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button2.Location = new System.Drawing.Point(101, 113);
+            this.button2.Location = new System.Drawing.Point(101, 121);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(135, 49);
+            this.button2.Size = new System.Drawing.Size(135, 45);
             this.button2.TabIndex = 1;
             this.button2.Text = "Lucinda";
             this.button2.UseVisualStyleBackColor = true;
@@ -59,9 +61,9 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button3.Location = new System.Drawing.Point(101, 196);
+            this.button3.Location = new System.Drawing.Point(101, 205);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(135, 50);
+            this.button3.Size = new System.Drawing.Size(135, 45);
             this.button3.TabIndex = 2;
             this.button3.Text = "Swap!";
             this.button3.UseVisualStyleBackColor = true;
@@ -69,7 +71,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(101, 291);
+            this.button4.Location = new System.Drawing.Point(101, 289);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(135, 45);
             this.button4.TabIndex = 3;
@@ -77,11 +79,34 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(101, 373);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(135, 45);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button6.Location = new System.Drawing.Point(101, 457);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(135, 45);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "程式碼磁貼";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(345, 416);
+            this.ClientSize = new System.Drawing.Size(335, 606);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -98,6 +123,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- Elephant/Elephant/Form1.cs
+++ Elephant/Elephant/Form1.cs
@@ -61,5 +61,57 @@ namespace Elephant
             lloyd.EarSize = 4321;
             lloyd.WhoAmI();
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Elephant[] elephants = new Elephant[7];
+            elephants[0] = new Elephant() { Name = "Lloyd", EarSize = 40 };
+            elephants[1] = new Elephant() { Name = "Lucinda", EarSize = 33 };
+            elephants[2] = new Elephant() { Name = "Larry", EarSize = 42 };
+            elephants[3] = new Elephant() { Name = "Lucille", EarSize = 32 };
+            elephants[4] = new Elephant() { Name = "Lars", EarSize = 44 };
+            elephants[5] = new Elephant() { Name = "Linda", EarSize = 37 };
+            elephants[6] = new Elephant() { Name = "Humphrey", EarSize = 45 };
+            Elephant biggestEars = elephants[0];
+            for(int i = 1; i < elephants.Length; i++)
+            {
+                if(elephants[i].EarSize > biggestEars.EarSize)
+                {
+                    biggestEars = elephants[i];
+                }
+            }
+            MessageBox.Show(biggestEars.EarSize.ToString());
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string[] islands = new string[4];
+            islands[0] = "Bermuda";
+            islands[1] = "Fiji";
+            islands[2] = "Azores";
+            islands[3] = "Cozumel";
+
+            int[] index = new int[4];
+            index[0] = 1;
+            index[1] = 3;
+            index[2] = 0;
+            index[3] = 2;
+
+            int y = 0;
+            int refNum;
+            string result = "";
+            while (y < 4)
+            {
+                refNum = index[y];
+                result += "\nisland= ";
+                result += islands[refNum];
+                //MessageBox.Show(result); 按一次做一次              
+                y = y + 1;
+
+            }
+
+            MessageBox.Show(result);
+
+        }
     }
 }
